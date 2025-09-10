cdbfce02147c8a57c83c6dca4eac965aa45f54b9
VictorPereira2525 <victor.pereira20@escola.pr.gov.br>
2025-09-10 20:58:10 -0300

    ok

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaprincipal=document.querySelector(".caixa-principal");
+const caixaperguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternaivas");
+const caixaresultado=document.querySelector(".caixa-rultado");
+const txtoresultado=document.querySelector("texto-resukltado");
+
+const perguntas = [
+    {
+     enunciado:"assim que do colégio você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e aúdios.Qual é o seu primeiro pensamento?",
+     alternativas:[
+        {
+            texto:"isso é assustador!",
+            afimacao:"Noinício, eu ficaria com medo do que essa tecnologia pode fazer."
+            
+        },
+        {
+            
+        }
+     ]
+
+    }
+]
